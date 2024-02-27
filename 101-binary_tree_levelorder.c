2ee1e31cd9fd2cea4c68bfd5840af3053a774535
Abdelrahman <abdelrahmanamr.egypt@gmail.com>
2024-02-27 12:11:09 -0800

    Change logic

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,44 +1,63 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_levelorder - use function on tree's node going by levels
- * @tree: root
- * @func: function
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height of
+ *
+ * Return: the height of the tree. If tree is NULL, return 0
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left > right)
+		return (1 + left);
+	return (1 + right);
+}
+
+/**
+ * binary_tree_level - perform a function on a specific level of a binary tree
+ * @tree: pointer to the root of the tree
+ * @l: level of the tree to perform a function on
+ * @func: function to perform
+ *
+ * Return: void
+*/
+
+void binary_tree_level(const binary_tree_t *tree, size_t l, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	if (l == 1)
+		func(tree->n);
+	else if (l > 1)
+	{
+		binary_tree_level(tree->left, l - 1, func);
+		binary_tree_level(tree->right, l - 1, func);
+	}
+}
+
+/**
+ * binary_tree_levelorder - traverses a binary tree using level-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node.
+ * The value in the node must be passed as a parameter to this function
+ *
+ * Return: void
 */
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	int SIZE = 512;
-	binary_tree_t **array = malloc(sizeof(array) * SIZE);
-	int head = 1;
-	int tail = 0;
-	int size = 1;
+	size_t height, i;
 
-	if (!array)
+	if (!tree || !func)
 		return;
-	if (!tree && !func)
-	{
-		free(array);
-		return;
-	}
-	array[0] = (binary_tree_t *) tree;
-	while (size)
-	{
-		if (array[tail])
-		{
-			func(array[tail]->n);
-			array[head] = array[tail]->left;
-			head = (head + 1) % SIZE;
-			array[head] = array[tail]->right;
-			head = (head + 1) % SIZE;
-			size++;
-		}
-		tail = (tail + 1) % SIZE;
-		while (size && !array[tail])
-		{
-			tail = (tail + 1) % SIZE;
-			size--;
-		}
-	}
-	free(array);
+	height = binary_tree_height(tree);
+	for (i = 1; i <= height; i++)
+		binary_tree_level(tree, i, func);
 }
