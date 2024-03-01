7dcf2abc00ba2e173bd3d06353a773fb73ef271a
Abdelrahman <abdelrahmanamr.egypt@gmail.com>
2024-02-29 17:11:57 -0800

    add check for middle nodes

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -11,7 +11,11 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left && tree->right)
+	{
+		if (tree->n <= tree->left->n || tree->n <= tree->right->n)
+			return (0);
 		return (binary_tree_is_heap(tree->left) && binary_tree_is_heap(tree->right));
+	}
 	if (tree->left || tree->right)
 		return (0);
 	return (tree->parent ? tree->parent->n > tree->n ? 1 : 0 : 1);
